e3bd54bcf5b9595e28eaca008041a7c3c508d742
sivasreethanigaivelu <sivasree.thanigaivelu@verinite.com>
2026-07-05 15:28:57 +0530

    Delete AccountService/AccountService directory

--- AccountService/AccountService/src/main/java/com/example/AccountService/exception/AccountNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.AccountService.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
--- AccountService/AccountService/src/main/java/com/example/AccountService/exception/DuplicateAccountException.java
+++
@@ -1,8 +0,0 @@
-package com.example.AccountService.exception;
-
-public class DuplicateAccountException extends RuntimeException {
-
-    public DuplicateAccountException(String message) {
-        super(message);
-    }
-}
